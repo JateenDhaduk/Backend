206e48126569f729b7ba883b3241923a22e3ae58
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-19 17:44:58 +0530

    git init

---
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/jwtRefreshdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtRefreshdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jwtRefreshdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(jwtRefreshdemoApplication.class, args);
+	}
+
+}
---
+++ jwtRefreshdemo/src/test/java/com/example/jwtRefreshdemo/JwtRefreshdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtRefreshdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRefreshdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
