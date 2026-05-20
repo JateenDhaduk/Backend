3768269c7c7c1f94fb876209e7a5df531ac9c53c
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-20 12:27:20 +0530

    add jwt util class for access and refresh token

---
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/security/JwtUtils.java
@@ -0,0 +1,86 @@
+package com.example.jwtRefreshdemo.security;
+
+import com.example.jwtRefreshdemo.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration-ms}")
+    private int jwtExpirationMils;
+
+    public String generateAccessToken(UserDetails userDetails){
+        Map<String,Object> extraClaims = new HashMap<>();
+
+        String userRole = userDetails.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse("UNASSIGNED");
+
+        extraClaims.put("role" , userRole);
+        return buildAccessToken(extraClaims,userDetails);
+    }
+
+    public String buildAccessToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date( new Date().getTime() + jwtExpirationMils))
+                .signWith(Key())
+                .compact();
+
+    }
+
+    public String buildRefreshToken(String email){
+        String refreshToken = UUID.randomUUID().toString();
+        return refreshToken;
+    }
+
+    public SecretKey Key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public <T> T extraClaims(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return  Jwts.parser()
+                .verifyWith(Key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUserName(String accessToken){
+        return extraClaims(accessToken,Claims::getSubject);
+    }
+
+    public boolean isTokenExpired(String accessToken){
+        return return extractExpiration(accessToken).before(new Date());
+    }
+
+    private Date extractExpiration(String accessToken) {
+        return extraClaims(accessToken ,Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String accessToken,UserDetails userDetails)throws Exception{
+        final String username = extractUserName(accessToken);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(accessToken);
+    }
+}
