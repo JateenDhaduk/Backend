4b6d2f7b214548dcc89c5c07c0e1b9ccf4510b97
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-20 18:53:52 +0530

    add costom jwt authetication filter inside security filter chain

--- jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/security/AuthSecurity.java
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/security/AuthSecurity.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -29,9 +30,14 @@ public class AuthSecurity {
                 csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(HttpMethod.POST,"/auth/user/register","/auth/user/login").permitAll()
+                        auth -> auth.requestMatchers(
+                                        HttpMethod.POST
+                                        ,"/auth/user/register"
+                                        , "/auth/user/login")
+                                .permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
