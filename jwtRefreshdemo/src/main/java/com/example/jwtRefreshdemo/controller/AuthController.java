2b99e39124001481a6c6cffd78e8e600ff797f4d
JateenDhaduk <jateendhaduk456@gmail.com>
2026-05-20 18:51:36 +0530

    add login endpoint

--- jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/controller/AuthController.java
+++ jwtRefreshdemo/src/main/java/com/example/jwtRefreshdemo/controller/AuthController.java
@@ -1,8 +1,7 @@
 package com.example.jwtRefreshdemo.controller;
 
-import com.example.jwtRefreshdemo.dto.ApiUserResponse;
-import com.example.jwtRefreshdemo.dto.UserRegisterRequest;
-import com.example.jwtRefreshdemo.dto.UserRegisterResponse;
+import com.example.jwtRefreshdemo.dto.*;
+import com.example.jwtRefreshdemo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth/user")
 @RequiredArgsConstructor
 public class AuthController {
-    private final UserServics userServics;
+    private final UserService userService;
 
     @PostMapping("/register")
     @Operation(summary = "create a new user" , description = "Endpoint for user register")
@@ -27,7 +26,22 @@ public class AuthController {
             @ApiResponse(responseCode = "400", description = "Invalid input data"),
             @ApiResponse(responseCode = "409", description = "Email or username already exists")
     })
-    public ResponseEntity<ApiUserResponse<UserRegisterResponse>> registerUser(@Valid @RequestBody UserRegisterRequest request){}
-        UserRegisterResponse response = userServics.registerUser(request);
-         return ResponseEntity.ok(ApiUserResponse.success(201, "Account created successfully", response));
+    public ResponseEntity<ApiUserResponse<UserRegisterResponse>> registerUser(@Valid @RequestBody UserRegisterRequest request){
+        UserRegisterResponse response = userService.registerUser(request);
+        return ResponseEntity.ok(ApiUserResponse.success(200,"Account created Successfully",response));
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "Login a user", description = "Endpoint to login a user with email and password.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User logged in successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid credentials")
+    })
+    public ResponseEntity<ApiUserResponse<UserLoginResponse>> loginUser(@Valid @RequestBody UserLoginRequest request) {
+        // Placeholder for user login logic
+        UserLoginResponse authResponse = userService.loginUser(request);
+        return ResponseEntity.ok(ApiUserResponse.success("Login successful", authResponse));
+    }
+
 }
